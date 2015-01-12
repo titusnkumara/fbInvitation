2f1da270085eb98fcc289ec198eff5fb0610bbf8
titus nanda kumara <titus.nandakumara@gmail.com>
2015-01-12 16:08:53 +0530

    added try catch blocks

--- facebook.js
+++ facebook.js
@@ -1,7 +1,7 @@
 (function addfriends(){
 	
-	var scrollingTimeout =  20;
-	var iterationTime = 1000;
+	var scrollingTimeout =  15;
+	var iterationTime = 2000;
 	var percentage=0;
 	function openpopup(content){ 
 		winpops=window.open('',"test","fullscreen=no,toolbar=yes,status=yes, " +
@@ -15,24 +15,33 @@
 	var a = document.getElementsByClassName("_5tee")[0];
 	function scroll(){
 		a.getElementsByClassName("uiScrollableAreaWrap scrollable")[0].scrollTop = 9999999;
-		var writeString = "wait.... scrolling "+String(start*percentage)+"%"
+		var writeString = "wait.... scrolling "
 		popup.document.write("<p>"+writeString+"</p>");
 		console.log(writeString);
 		percentage++;
 	}
 	
 	function sendRequests(){
+		console.log("Sending requests started");
 		var b =document.getElementsByClassName("_5l6a")[0];
 		divs = b.getElementsByClassName("_42ef");
 		length = divs.length;
-
+		console.log(String(length)+" captured");
+		var countWithoutError = 0;
 		for(var x=2;x<length;x=x+2){
 			//edit here
-			divs[x].getElementsByTagName('button')[0].click();
-			var writeProgress = "Sent to "+divs[x].getElementsByTagName('strong')[0].innerText + " count "+String(x/2);
-			popup.document.write("<p>"+writeProgress+"</p>");
-			console.log(writeProgress);
+			try{
+				divs[x].getElementsByTagName('button')[0].click();
+				var writeProgress = "Sent to "+divs[x].getElementsByTagName('strong')[0].innerText + " count "+String(x/2);
+				popup.document.write("<p>"+writeProgress+"</p>");
+				console.log(writeProgress);
+				countWithoutError++;
+			}catch(err){
+				console.log(err);
+				continue;
+			}
 		}
+		console.log(String(countWithoutError)+" Sent without errors");
 	}
 	// scroll to all members
 	for (var start = 1; start < scrollingTimeout; start++){
@@ -43,7 +52,7 @@
 	//send requests
 	setTimeout(function () {
 					sendRequests();
-				}, iterationTime * (scrollingTimeout+1));
+				}, iterationTime * (scrollingTimeout+2));
 
 	
 	
